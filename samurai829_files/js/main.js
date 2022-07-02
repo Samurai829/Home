ff1e5d12c0db75b0e3134018cf9f98743d7a63bf
Samurai829 <stevengarcia.94@hotmail.com>
2022-07-01 23:24:04 -0400

    update 2.1

--- samurai829_files/js/main.js
+++ samurai829_files/js/main.js
@@ -76,12 +76,12 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2700,
-    delay: 300,
+    duration: 2000,
+    delay: 250,
 //     reset: true
 });
 
 sr.reveal('.home__data, .section_title, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 200}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .projects__data, .projects__img, .contacts__social' ,{interval: 200}); 
